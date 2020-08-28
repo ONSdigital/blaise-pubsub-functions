62a88410fa44ce70aceb62b780acfb4dede9a3ff
Jamie Kerr <jamiestephenkerr@gmail.com>
2020-08-28 17:26:20 +0100

    updates

--- go/replay_deadletter_messages_function.go
+++ go/replay_deadletter_messages_function.go
@@ -9,33 +9,60 @@ import (
 
 	"cloud.google.com/go/pubsub"
 	"google.golang.org/api/option"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var (
-	topic              *pubsub.Topic
-	googleCloudProject = "ons-blaise-dev-jam39"
-	topicId            = "blaise-deadletter-test-topic"
-	subscriptionId     = "blaise-deadletter-test-subscription"
-	sourceSubscription = "subscription-tel"
+	topic                      *pubsub.Topic
+	googleCloudProject         = "ons-blaise-dev-jam39"
+	topicId                    = "blaise-deadletter-test-topic"
+	subscriptionId             = "blaise-deadletter-test-subscription"
+	sourceSubscription         = "subscription-tel"
+	lastTimeOfRecordedActivity = time.Now()
 )
 
-const maxMessages = 10
 const subscriptionSourceAttributeName = "CloudPubSubDeadLetterSourceSubscription"
 const credentialsPath = "/users/jamiekerr/Downloads/ons-blaise-dev-jam39-6de66e990a49.json"
-const timeoutInSeconds = 30 * time.Second
+const timeout = 30 * time.Second
 
 func main() {
-	log.Println("Started")
+	fmt.Println("Started")
 
-	err := processMessagesInSubscription(subscriptionId, maxMessages)
+	err := processMessagesInSubscriptionSync(subscriptionId)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("Finished")
+	fmt.Println("Finished")
 }
 
-func processMessagesInSubscription(subscriptionId string, limitMessages int) error {
+func cancelContextDueToInactivity(cancel context.CancelFunc) {
+
+	for {
+		time.Sleep(10 * time.Second)
+
+		fmt.Println("Last activity = " + lastTimeOfRecordedActivity.String())
+		diff := time.Now().Sub(lastTimeOfRecordedActivity)
+		if diff.Seconds() > 30 {
+			fmt.Println("Cancelling due to inactivity")
+			cancel()
+		}
+	}
+}
+
+var processedMessages = 0
+
+func testBgTask(cancel context.CancelFunc) {
+
+	for {
+		time.Sleep(5 * time.Second)
+
+		fmt.Println("Processed messages " + fmt.Sprint(processedMessages))
+	}
+}
+
+func processMessagesInSubscription(subscriptionId string) error {
 	ctx := context.Background()
 
 	client, err := pubsub.NewClient(ctx, googleCloudProject, option.WithCredentialsFile(credentialsPath))
@@ -45,33 +72,31 @@ func processMessagesInSubscription(subscriptionId string, limitMessages int) err
 	}
 
 	var mu sync.Mutex
-	processedMessages := 0
+
 	sub := client.Subscription(subscriptionId)
 
-	cctx, cancel := context.WithTimeout(ctx, timeoutInSeconds)
+	cctx, cancel := context.WithTimeout(ctx, timeout)
+
+	//go cancelContextDueToInactivity(cancel)
 
 	err = sub.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
+		processedMessages++
 		mu.Lock()
 		defer mu.Unlock()
 
+		lastTimeOfRecordedActivity = time.Now()
 		if messageMatchesAttributeFilter(msg, subscriptionSourceAttributeName, sourceSubscription) {
-			log.Println("Got message with id" + msg.ID + " data " + string(msg.Data))
-			processedMessages++
+			fmt.Println("Got message with id" + msg.ID + " data " + string(msg.Data) + " at - " + time.Now().String())
+
 			//TODO: publish message to topic to replay
 			msg.Ack()
 		} else {
-			log.Println("ignored message with id" + msg.ID + " data " + string(msg.Data))
-			msg.Nack()
-		}
-
-		if processedMessages == limitMessages {
-			log.Println("Cancelled - limit reached")
-			cancel()
+			fmt.Println("ignored message with id" + msg.ID + " data " + string(msg.Data) + " at - " + time.Now().String())
+			msg.Ack()
 		}
 
 		select {
 		case <-ctx.Done():
-			log.Println("Cancelled - timout period reached")
 			cancel()
 		}
 	})
@@ -80,6 +105,7 @@ func processMessagesInSubscription(subscriptionId string, limitMessages int) err
 		return fmt.Errorf("Receive: %v", err)
 	}
 
+	fmt.Println("processed " + fmt.Sprint(processedMessages) + " messages")
 	return nil
 }
 
@@ -90,5 +116,73 @@ func messageMatchesAttributeFilter(msg *pubsub.Message, attributeName, attribute
 		}
 	}
 
+	return false
+}
+
+func processMessagesInSubscriptionSync(subID string) error {
+
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, googleCloudProject, option.WithCredentialsFile(credentialsPath))
+	if err != nil {
+		return fmt.Errorf("pubsub.NewClient: %v", err)
+	}
+	defer client.Close()
+
+	sub := client.Subscription(subID)
+
+	// Turn on synchronous mode. This makes the subscriber use the Pull RPC rather
+	// than the StreamingPull RPC, which is useful for guaranteeing MaxOutstandingMessages,
+	// the max number of messages the client will hold in memory at a time.
+	sub.ReceiveSettings.Synchronous = true
+	sub.ReceiveSettings.MaxOutstandingMessages = 5
+
+	// Receive messages for 5 seconds.
+	ctx, cancel := context.WithTimeout(ctx, 300*time.Second)
+	defer cancel()
+
+	// Create a channel to handle messages to as they come in.
+	messageChannel := make(chan *pubsub.Message)
+	defer close(messageChannel)
+
+	i := 0
+
+	lastTimeOfRecordedActivity = time.Now()
+	go cancelContextDueToInactivity(cancel)
+
+	var listIds []string
+	// Handle individual messages in a goroutine.
+	go func() {
+		for msg := range messageChannel {
+			lastTimeOfRecordedActivity = time.Now()
+
+			if stringInSlice(msg.ID, listIds) {
+				fmt.Println("duplicate message " + string(msg.Data))
+				msg.Ack()
+			} else {
+				i++
+				fmt.Println("Got message " + string(msg.Data))
+			}
+		}
+	}()
+
+	// Receive blocks until the passed in context is done.
+	err = sub.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
+		messageChannel <- msg
+	})
+	if err != nil && status.Code(err) != codes.Canceled {
+		return fmt.Errorf("Receive: %v", err)
+	}
+
+	fmt.Println("processed " + fmt.Sprint(i) + " messages")
+
+	return nil
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
 	return false
 }
--- tests/filter_functions_tests_integration.py
+++ tests/filter_functions_tests_integration.py
@@ -12,13 +12,13 @@ def test_filter_messages_by_source_returns_results_when_messages_matching_filter
     topicId = 'blaise-deadletter-test-topic'
     subscriptionId = 'blaise-deadletter-test-subscription'
 
-    delete_subscription(subscriptionId)
-    delete_topic(topicId)
+    #delete_subscription(subscriptionId)
+    #delete_topic(topicId)
     create_topic(topicId)
     create_subscription(topicId, subscriptionId)
 
     i = 1
-    while i < 200:
+    while i < 100:
         publish_message(topicId, 'Test body ' + str(i), 'subscription-val')
         i += 1
 
@@ -26,11 +26,11 @@ def test_filter_messages_by_source_returns_results_when_messages_matching_filter
 
     
     #act
-    result = filter_messages_by_source_subscription(subscriptionId, 2, filter)
+    """result = filter_messages_by_source_subscription(subscriptionId, 2, filter)
 
     delete_subscription(subscriptionId)
     delete_topic(topicId)
 
     #assert
-    assert len(result) == 1
+    assert len(result) == 1"""
     
